39efbb2590478d4394d07f00e7d5f4e0bf2dd387
Jaydesire <elgovsky@ya.ru>
2023-03-06 15:35:57 +0300

    Added UserRepository realization

--- src/main/java/ru/netology/serviceauthorizationapplication/UserRepository.java
+++ src/main/java/ru/netology/serviceauthorizationapplication/UserRepository.java
@@ -1,12 +1,25 @@
 package ru.netology.serviceauthorizationapplication;
 
 import org.springframework.stereotype.Repository;
+import ru.netology.serviceauthorizationapplication.exceptions.UnauthorizedUser;
 
 import java.util.*;
 @Repository
 public class UserRepository {
+    private Map<String, User> users = new HashMap<>();
+
+    public UserRepository() {
+        users.put("user1", new User("user1", "pass1", Authorities.READ));
+        users.put("user2", new User("user2", "pass2", Authorities.READ, Authorities.WRITE));
+    }
 
     public List<Authorities> getUserAuthorities(String user, String password) {
-        return new ArrayList<>();
+        if(!users.containsKey(user)) {
+            throw new UnauthorizedUser("Unknown user");
+        }
+        if(!users.get(user).getPassword().equals(password)) {
+            throw new UnauthorizedUser("Wrong password");
+        }
+        return users.get(user).getAuthorities();
     }
 }
